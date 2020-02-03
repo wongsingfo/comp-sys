4ac91e71e0c137fce9d4b1ef46935c8ca6cd4647
Wang Chengke <wongck@pku.edu.cn>
2020-02-03 18:50:30 +0800

    [config] add graphviz

---
+++ assets/js/graphviz-render.js
@@ -0,0 +1,26 @@
+var debug;
+
+(function() {
+  let inputs = document.getElementsByClassName("graphviz")
+  var viz = new Viz();
+
+  for (let item of inputs) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/Node
+    (function() {
+      let self = item;
+
+      viz.renderSVGElement(item.textContent)
+      .then(function(element) {
+        self.parentElement.insertBefore(element, self);
+        self.parentElement.removeChild(self);
+      })
+      .catch(error => {
+        /* Create a new Viz instance (@see Caveats page for more info) */
+        viz = new Viz();
+        self.textContent = error;
+        /* Possibly display the error */
+        console.error(error);
+      });
+    })()
+  }
+})() 
